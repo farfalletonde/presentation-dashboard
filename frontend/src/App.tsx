98b383a54fe628b80806fb015544fbaef361515c
farfalletonde <haybak21@gmail.com>
2024-11-22 01:17:51 +0300

    router setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/auth/login" element={<Login />} />
+      <Route path="/auth/signup" element={<Signup />} />
+    </Routes>
+  );
 }
 
 export default App;

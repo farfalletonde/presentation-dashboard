652ad3f3f73f18478085a7cc766dc29821d8f4a0
farfalletonde <haybak21@gmail.com>
2024-11-28 09:38:00 +0300

    user-select: none

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,7 +1,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios, { AxiosResponse } from "axios";
 
-axios.defaults.baseURL = 'http://localhost:5001/api/';
+axios.defaults.baseURL = '/api/';
 
 async function getUserPayload(): Promise<string | null> {
   try {
